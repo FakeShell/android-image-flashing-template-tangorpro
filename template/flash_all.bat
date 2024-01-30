c7f686ed25fb527984296d2b1c51d08fb1336fe5
Bardia Moshiri <fakeshell@bardia.tech>
2024-01-30 12:51:44 -0500

    add a vbmeta image for flashing
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- template/flash_all.bat
+++ template/flash_all.bat
@@ -1,5 +1,7 @@
 fastboot flash boot data/boot.img
 fastboot flash userdata data/userdata.img
+fastboot --disable-verity --disable-verification flash vbmeta_a data/vbmeta.img
+fastboot --disable-verity --disable-verification flash vbmeta_b data/vbmeta.img
 
 fastboot reboot
 
--- template/flash_all.sh
+++ template/flash_all.sh
@@ -2,6 +2,8 @@
 
 fastboot flash boot data/boot.img
 fastboot flash userdata data/userdata.img
+fastboot --disable-verity --disable-verification flash vbmeta_a data/vbmeta.img
+fastboot --disable-verity --disable-verification flash vbmeta_b data/vbmeta.img
 
 fastboot reboot
 
